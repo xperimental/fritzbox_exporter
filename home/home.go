a6aaf36aefe5856ff341d3684731927f20ed0bf7
Robert Jacob <xperimental@solidproject.de>
2017-01-08 23:20:28 +0100

    Implement parsing of the smart home HTML.

--- home/home.go
+++ home/home.go
@@ -2,11 +2,23 @@
 package home
 
 import (
+	"errors"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
+	htmlquery "github.com/antchfx/xquery/html"
+	"golang.org/x/net/html"
+
 	"github.com/prometheus/client_golang/prometheus"
+)
+
+const (
+	homeURLFormat = "http://%s/net/home_auto_overview.lua?sid=%s&update=uiSmarthomeTables&view=&xhr=1"
 )
 
 var (
@@ -16,8 +28,12 @@ var (
 	}
 
 	tempDesc = prometheus.NewDesc(
-		"fritzbox_home_temperature_celsius",
+		"fritzbox_home_current_temperature_celsius",
 		"Current temperature measurement of home automation device in celsius.",
+		varLabels, nil)
+	targetTempDesc = prometheus.NewDesc(
+		"fritzbox_home_target_temperature_celsius",
+		"Target temperature setting of home automation device in celsius.",
 		varLabels, nil)
 )
 
@@ -61,6 +77,7 @@ func (c *homeCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.UpMetric.Desc()
 	ch <- c.AuthMetric.Desc()
 	ch <- tempDesc
+	ch <- targetTempDesc
 }
 
 func (c *homeCollector) Collect(ch chan<- prometheus.Metric) {
@@ -79,5 +96,135 @@ func (c *homeCollector) Collect(ch chan<- prometheus.Metric) {
 		c.AuthMetric.Set(1)
 	}
 	ch <- c.AuthMetric
+
+	home, err := c.getHomeData()
+	if err != nil {
+		log.Printf("Error getting home data: %s", err)
+
+		c.UpMetric.Set(0)
+		ch <- c.UpMetric
+		return
+	}
+	c.UpMetric.Set(1)
 	ch <- c.UpMetric
+
+	for _, th := range home.Thermostats {
+		labels := []string{
+			c.Hostname,
+			th.Name,
+		}
+
+		sendMetric(ch, tempDesc, th.CurrentTemperature, labels)
+		sendMetric(ch, targetTempDesc, th.TargetTemperature, labels)
+	}
+}
+
+func sendMetric(ch chan<- prometheus.Metric, desc *prometheus.Desc, value float64, labels []string) {
+	m, err := prometheus.NewConstMetric(desc, prometheus.GaugeValue, value, labels...)
+	if err != nil {
+		log.Printf("Error creating metric %s: %s", tempDesc.String(), err)
+		return
+	}
+	ch <- m
+}
+
+type homeData struct {
+	Thermostats []thermostat
+}
+
+type thermostat struct {
+	Name               string
+	CurrentTemperature float64
+	TargetTemperature  float64
+}
+
+func (c *homeCollector) getHomeData() (homeData, error) {
+	var result homeData
+	url := fmt.Sprintf(homeURLFormat, c.Hostname, c.Sid)
+	res, err := c.Client.Get(url)
+	if err != nil {
+		return result, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return result, fmt.Errorf("invalid status code: %d", res.StatusCode)
+	}
+
+	if err := parseHomeData(&result, res.Body); err != nil {
+		return result, err
+	}
+
+	return result, nil
+}
+
+func parseHomeData(result *homeData, r io.Reader) error {
+	contextNode := &html.Node{
+		Type: html.ElementNode,
+	}
+	nodes, err := html.ParseFragment(r, contextNode)
+	if err != nil {
+		return err
+	}
+
+	for _, node := range nodes {
+		if node.Type == html.ElementNode && node.Data == "table" {
+			return parseTableNode(result, node)
+		}
+	}
+
+	return errors.New("no table node found")
+}
+
+func parseTableNode(result *homeData, table *html.Node) error {
+	items := htmlquery.Find(table, "//tr")
+itemLoop:
+	for _, item := range items {
+		for _, a := range item.Attr {
+			if a.Key == "class" && a.Val == "thead" {
+				continue itemLoop
+			}
+		}
+
+		nameNode := htmlquery.FindOne(item, "//td[@class='name cut_overflow']/span/text()")
+		if nameNode == nil {
+			log.Println("Warning: no name node found")
+			continue
+		}
+
+		tempNode := htmlquery.FindOne(item, "//td[@class='temperature']/text()")
+		if tempNode == nil {
+			log.Println("Warning: no temperature node found")
+			continue
+		}
+
+		targetTempNodes := htmlquery.Find(item, "//td[@class='target_temperature']//span[@class='numdisplay']/span/text()")
+		if len(targetTempNodes) != 2 {
+			log.Printf("Warning: not two elements in target temperature %d", len(targetTempNodes))
+			continue
+		}
+
+		name := strings.TrimSpace(nameNode.Data)
+		tempStr := strings.Replace(strings.Trim(tempNode.Data, " \n°C"), ",", ".", 1)
+		targetTempStr := strings.Replace(strings.TrimSpace(targetTempNodes[0].Data), ",", ".", 1)
+
+		temp, err := strconv.ParseFloat(tempStr, 64)
+		if err != nil {
+			log.Printf("Error parsing temperature: %s", err)
+			continue
+		}
+
+		targetTemp, err := strconv.ParseFloat(targetTempStr, 64)
+		if err != nil {
+			log.Printf("Error parsing target temperature: %s", err)
+			continue
+		}
+
+		result.Thermostats = append(result.Thermostats, thermostat{
+			Name:               name,
+			CurrentTemperature: temp,
+			TargetTemperature:  targetTemp,
+		})
+	}
+	return nil
 }
